94ed5716e07370387646ed6c8902540ef95d0b7d
Bongus <bongleebong@gmail.com>
2022-12-20 22:58:12 +0000

    Code added

---
+++ WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel.Helpers
+{
+    public class AccuWeatherHelper
+    {
+        public const string apiKey = "vMe4bnPWPw007GizM1EgwylgR13v5yqS";
+        public const string baseURL = "http://dataservice.accuweather.com";
+        public const string autoCompleteEndpoint = "locations/v1/cities/autocomplete/?apikey={0}&q={1}";
+        public const string currentConditionsEndpoint = "currentconditions/v1/{0}?apikey={1}";
+
+        public static async Task<List<City>> GetCities(string query)
+        {
+            List<City> cities = new List<City>();
+            string requestURL = baseURL + string.Format(autoCompleteEndpoint, apiKey, query);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(requestURL);
+                string json = await response.Content.ReadAsStringAsync();
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
+
+        public static async Task<CurrentConditions> GetCurrentConditions(string cityKey)
+        {
+            CurrentConditions currentConditions = new CurrentConditions();
+
+            string requestURL = baseURL + string.Format(currentConditionsEndpoint, cityKey, apiKey);
+
+            using (HttpClient client = new HttpClient()) 
+            { 
+                var response = await client.GetAsync(autoCompleteEndpoint);
+                string json = await response.Content.ReadAsStringAsync();
+                currentConditions = JsonConvert.DeserializeObject<List<CurrentConditions>>(json).FirstOrDefault();
+            }
+
+            return currentConditions;
+        }
+    }
+}
---
+++ WeatherApp/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherViewModel : INotifyPropertyChanged
+    {
+        private string query;
+        public string Query
+        {
+            get { return query; }
+            set 
+            { 
+                query = value;
+                // OnPropertyChanged only accepts strings and it has to match the name of the Property. In this case, 'Query'.
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentConditions currentConditions;
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set 
+            { 
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set 
+            { 
+                selectedCity = value; 
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+        //public WeatherViewModel()
+        //{
+        //    selectedCity = new City
+        //    {
+        //        LocalizedName = "New York"
+        //    };
+        //}
+
+        // Trigger this event to notify that the prop has changed (whenever setter is called).
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
